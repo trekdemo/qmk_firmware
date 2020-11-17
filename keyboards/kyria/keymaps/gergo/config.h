d46e93b11efcc0a35f6271cb092fb5d4f62c91c8
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2020-11-17 14:29:20 +0100

    Go back to 200ms TAP_TERM

--- keyboards/kyria/keymaps/gergo/config.h
+++ keyboards/kyria/keymaps/gergo/config.h
@@ -3,7 +3,7 @@
 // Recommended for heavy chording.
 #define QMK_KEYS_PER_SCAN 4
 
-#define TAPPING_TERM 185
+#define TAPPING_TERM 200
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 #undef PERMISSIVE_HOLD
@@ -17,15 +17,12 @@
 #endif
 
 #ifdef RGBLIGHT_ENABLE
+  #define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19}
+  // RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,19,18,17,16,15,14,13,12,11,10}
   #define RGBLIGHT_ANIMATIONS
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
+  // https://docs.qmk.fm/#/feature_rgblight?id=lighting-layers
+  #define RGBLIGHT_LAYERS
 #endif
-
-// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-#define SPLIT_USB_DETECT
-#define NO_USB_STARTUP_CHECK
-
-// https://docs.qmk.fm/#/feature_rgblight?id=lighting-layers
-#define RGBLIGHT_LAYERS
